3373e9da32da812bbacf1575331c16e564901633
Kostiantyn Verbitskyi <kostya.verbitsky15@gmail.com>
2025-12-14 16:58:05 +0200

    Add JPA entities for database tables

---
+++ src/main/java/com/webjava/lab1/entity/CategoryEntity.java
@@ -0,0 +1,42 @@
+package com.webjava.lab1.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CategoryEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "categories_seq")
+  @SequenceGenerator(name = "categories_seq", sequenceName = "categories_id_seq", allocationSize = 1)
+  private Long id;
+
+  @Column(name = "name", nullable = false, length = 100, unique = true)
+  private String name;
+
+  @Column(name = "description", length = 500)
+  private String description;
+
+  @OneToMany(mappedBy = "category")
+  @Builder.Default
+  private List<ProductEntity> products = new ArrayList<>();
+}
---
+++ src/main/java/com/webjava/lab1/entity/ProductEntity.java
@@ -0,0 +1,49 @@
+package com.webjava.lab1.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(
+    name = "products",
+    uniqueConstraints = @UniqueConstraint(columnNames = {"name", "category_id"}))
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProductEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "products_seq")
+  @SequenceGenerator(name = "products_seq", sequenceName = "products_id_seq", allocationSize = 1)
+  private Long id;
+
+  @Column(name = "name", nullable = false, length = 200)
+  private String name;
+
+  @Column(name = "description", length = 1000)
+  private String description;
+
+  @Column(name = "price", nullable = false, precision = 19, scale = 2)
+  private BigDecimal price;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "category_id", nullable = false)
+  private CategoryEntity category;
+}
